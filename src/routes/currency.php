70dc2acb63b44604a72da64aded422a7a729a21d
Nurik <nurik9293709@gmail.com>
2020-08-07 00:06:39 +0600

    routes

--- src/routes/currency.php
+++ src/routes/currency.php
@@ -2,7 +2,10 @@
 
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
-    'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
+    'middleware' => [
+        config('backpack.base.web_middleware', 'web'),
+        config('backpack.base.middleware_key', 'admin'),
+    ],
     'namespace'  => 'BrandStudio\Currency\Http\Controllers',
 ], function () { // custom admin routes
     Route::crud('currency', 'CurrencyCrudController');
